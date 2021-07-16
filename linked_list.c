f121cdced8131d3a47b239de47d32c3e6581064e
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2021-07-16 07:33:42 +0530

    test with insertion

--- linked_list.c
+++ linked_list.c
@@ -83,26 +83,13 @@ int Delete(int value, struct Node** head){
 
 }
 
-
+struct Node* head = NULL;
 int main()
 {
-    struct Node* head = NULL;
-    struct Node* second = NULL;
-    struct Node* third = NULL;
- 
-    head = (struct Node*)malloc(sizeof(struct Node));
-    second = (struct Node*)malloc(sizeof(struct Node));
-    third = (struct Node*)malloc(sizeof(struct Node));
- 
-    head->data = 1; 
-    head->next = second; 
-    second->data = 2;
- 
-    second->next = third;
- 
-
-    third->data = 3;
-    third->next = NULL;
+    
+    Insert(1,&head);
+    Insert(2,&head);
+    Insert(3,&head);
 
     printList(head);
  
